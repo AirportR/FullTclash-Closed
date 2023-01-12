fdca7a953cb781c8908d6fa540886879aa78242d
AirportR <airportroster@gmail.com>
2023-01-12 18:07:04 +0800

    Delete source files and provide executable files only

--- main.py
+++
@@ -1,8 +0,0 @@
-from pyrogram import Client
-from libs import bot
-from botmodule import init_bot
-
-if __name__ == "__main__":
-    app = Client("my_bot", proxy=init_bot.proxies, ipv6=False)
-    bot.loader(app)
-    app.run()
